e0e569c7983b447a0afaa2deb0ca251a68d7ed60
wood.wu <kilokingw@gmail.com>
2023-04-26 17:13:12 +0800

    Updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
+const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://xiangfulong.com.tw',
-  generateRobotsTxt: true, // (optional)
+  siteUrl: process.env.SITE_URL || 'https://xiangfulong.com.tw/',
+  exclude: ["/404"],
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/404"],
+      },
+      { userAgent: "*", allow: "/" },
+    ],
+
+  },
 }
